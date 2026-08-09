1e03b74246fe96b0225246d06a37084f3a7b68cb
Chengyuan Zhang <1136427613@qq.com>
2026-08-09 11:52:58 +0800

    feat(nvim/lsp): enable auto inlay hint

--- usr/nvim/lsp/default.nix
+++ usr/nvim/lsp/default.nix
@@ -8,6 +8,23 @@
     plugins = [
       {
         plugin = pkgs.vimPlugins.nvim-lspconfig;
+        type = "lua";
+        config = ''
+          vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client and client.server_capabilities.inlayHintProvider then
+                -- Enable inlay hints by default on attach
+                vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+              end
+            end,
+          })
+
+          vim.keymap.set('n', '<leader>th', function()
+            local current = vim.lsp.inlay_hint.is_enabled()
+            vim.lsp.inlay_hint.enable(not current)
+          end, { desc = 'Toggle Inlay Hints' })
+        '';
       }
     ];
   };

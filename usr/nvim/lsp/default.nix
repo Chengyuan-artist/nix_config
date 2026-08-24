4483a7f822a059ec39622ade2c34ec06dfce0205
Chengyuan Zhang <1136427613@qq.com>
2026-08-24 20:31:20 +0800

    nvim: lsp add diagnostics keymap

--- usr/nvim/lsp/default.nix
+++ usr/nvim/lsp/default.nix
@@ -25,6 +25,31 @@
         plugin = pkgs.vimPlugins.nvim-lspconfig;
         type = "lua";
         config = ''
+          vim.lsp.log.set_level("OFF")
+
+          -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+          vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+          vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count=-1, float=true, severity = { min = vim.diagnostic.severity.WARN } }) end)
+          vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count= 1, float=true, severity = { min = vim.diagnostic.severity.WARN } }) end)
+          vim.keymap.set('n', '[D', function() vim.diagnostic.jump({ count=-1, float=true, severity = { min = vim.diagnostic.severity.INFO } }) end)
+          vim.keymap.set('n', ']D', function() vim.diagnostic.jump({ count= 1, float=true, severity = { min = vim.diagnostic.severity.INFO } }) end)
+          vim.keymap.set('n', '<space>d', Snacks.picker.diagnostics_buffer)
+          vim.keymap.set('n', '<space>D', Snacks.picker.diagnostics)
+          vim.diagnostic.config({
+            float = {
+              source = true,  -- Show the source (LSP server name)
+            },
+            signs = {
+              text = {
+                -- Do not show 'H' in the sign/number column.
+                -- The main reason is harper_ls throws many hint diagnostics
+                [vim.diagnostic.severity.HINT] = "",
+              },
+            },
+          })
+
+          -- Use LspAttach autocommand to only map the following keys
+          -- after the language server attaches to the current buffer
           vim.api.nvim_create_autocmd('LspAttach', {
             callback = function(args)
               local client = vim.lsp.get_client_by_id(args.data.client_id)
